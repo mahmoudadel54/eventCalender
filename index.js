97256258ecfe53e77ee31c58adf883d60ebbb582
mahmoudadel54 <mahmoodmadel@gmail.com>
2020-11-10 00:08:51 +0200

    3rd commit

--- index.js
+++ index.js
@@ -29,6 +29,16 @@ app.use('/api/v1/user',userRoute)
 app.use('/api/v1/event',eventRoute)
 app.use('/api/v1/googlelogin',googleRoute)
 
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) =>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 // for page not found
 app.use((req,res,next)=>{
     const error = new Error("Page not found")
